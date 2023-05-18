1155915462b1a42a9c59de8da84b8b8fcff7002c
SalamandraDevs <salamandradevs@gmail.com>
2023-05-18 11:51:29 -0500

    Tesging some tsconfig options

--- packages/submodule/types/reserves.d.ts
+++
@@ -1,5 +0,0 @@
-import { Web3 } from 'web3';
-export declare namespace Reserves {
-    function getReservesByPairs(web3: Web3, pairs: Array<object>): Promise<void>;
-}
-//# sourceMappingURL=reserves.d.ts.map
